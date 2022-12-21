abf770ec130cbdf78e13f7182f0db7dd1788f5ba
HassaanKH8 <hassaankh8@gmail.com>
2022-12-21 21:26:51 +0500

    v1.1

--- main.js
+++ main.js
@@ -146,35 +146,5 @@ app.listen(process.env.PORT || 3000, function () {
         totaltasks.push(mytasks);
         totaltasks.push(myremtasks);
 
-        // loop to make my app running
-
-        var i = 1;
-
-        var http = require('http'); //importing http
-
-        function startKeepAlive() {
-            setInterval(function () {
-                var options = {
-                    host: 'spacedr.herokuapp.com',
-                    port: 80,
-                    path: '/'
-                };
-                http.get(options, function (res) {
-                    res.on('data', function (chunk) {
-                        try {
-                            // optional logging... disable after it's working
-                            console.log("HEROKU RESPONSE: " + chunk);
-                        } catch (err) {
-                            console.log(err.message);
-                        }
-                    });
-                }).on('error', function (err) {
-                    console.log("Error: " + err.message);
-                });
-            }, 10 * 60 * 1000); // load every 10 minutes
-        }
-
-        startKeepAlive();
-
     });
 });
